10a86c663e4e848111b3fa340210f1d620b1dc0b
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-08 21:30:57 +0100

    sending the mail

--- using .starttls()/auto_mail.py
+++ using .starttls()/auto_mail.py
@@ -3,4 +3,28 @@ import smtplib, ssl
 smtp_server = "smtp.gmail.com"
 port = 587 # for starttls
 sender_email = "ezeokewill@gmail.com"
+receiver_email = "fredapaul58@gmail.com"
 password = input("Type password and press Enter: ")
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+# Create a secure SSL Context
+context = ssl.create_default_context()
+
+# Try to log in to server and send mail
+try:
+    server = smtplib.SMTP(smtp_server, port)
+    server.ehlo() # optional because it is implicitly called by .starttls() and .sendmail(), if required
+    server.starttls(context=context) # securing the connection
+    server.ehlo() # also optional because it is implicitly called by .starttls() and .sendmail(), if required
+    server.login(sender_email, password)
+
+    #TODO: Send Email here
+    server.sendmail(sender_email, receiver_email, message)
+except Exception as e:
+    # Print an error message
+    print(e)
+finally:
+    server.quit()
--- using_SMTP_SSL()/auto_mail.py
+++ using_SMTP_SSL()/auto_mail.py
@@ -2,11 +2,20 @@ import smtplib, ssl
 
 port = 465 # For SSL connection
 password = input("Type your password and press Enter: ")
+smtp_server = "smtp.gmail.com"
+sender_email = "ezeokewill@gmail.com"
+receiver_email = "fredapaul58@gmail.com"
+message = """\
+    Subject: Hello From Python
+
+    This message is sent from Python.
+    """
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("ezeokewill@gmail.com", password)
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
 
     #TODO: SEND EMAIL
+    server.sendmail(sender_email, receiver_email, message)
